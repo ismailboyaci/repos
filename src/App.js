b54a29835ca2c8c721ecb0a10c180bdbea568b00
ismailboyaci <ism.byc@hotmail.com>
2022-05-31 17:35:13 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
    <Router>
-     <Route exact path="/"> <Login /> </Route>
-     <Route  path="/home"> <Home /> </Route>     
+     <Route exact path="/" ><Login /></Route> 
+     <Route  path="/home"> <Home /></Route>  
    </Router>
   );
 }
